44975502186974a8c41fd89392fe213073e290de
Serhii Savchak <itsavchak@gmail.com>
2024-03-11 19:56:40 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 import styles from './page.module.css';
-import GoogleComSection from '@/components/common/GoogleComSection/GoogleComSection';
-import Container from '@/components/common/Container/Container';
 
 const HomePage = () => {
   return (
     <>
-      <Container>
-        <div>HomePage</div>
-      </Container>
-      <GoogleComSection />
+      <div>HomePage</div>
     </>
   );
 };
