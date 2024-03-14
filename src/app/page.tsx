05fef3d5f07f0d2549d125fc632566e7568fb307
Serhii Savchak <itsavchak@gmail.com>
2024-03-14 17:51:39 +0200

    add fix to video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const HomePage = () => {
   return (
     <>
       {/* <div>HomePage</div> */}
-      {/* <VideoSection {...firstVideo} /> */}
+      {/* <VideoSection {...secondVideo} /> */}
       <QuestionSection />
       <TeamSection />
       <ContactUsSection />
--- src/utils/video.ts
+++ src/utils/video.ts
@@ -1,12 +1,12 @@
 export const firstVideo = {
-  className: 'first',
+  className: 'left',
   title: 'Die Energie der Zukunft!',
   link: 'https://www.youtube.com/embed/wR28cPmRdXs',
   text: 'PV-Anlagen sind der Schlüssel zum Geldsparen. In einem kurzen Video erzählt dir dein Helfer Mr. Energy genauer, wie PV-Anlagen funktionieren. Mr. Energy verrät dir, wie PV-Anlagen deinen Verbrauch senken und dir Gewinn verschaffen. Investiere eine Minute deiner Zeit und erlange den Schlüssel zu einer unabhängigen Zukunft mit WorkSET Energy.',
 };
 
 export const secondVideo = {
-  className: 'second',
+  className: 'right',
   title: 'Bleibe in den schwarzen Zahlen',
   text: 'Erfahre, wie das Kreditsystem Einsparungen bei Photovoltaikanlagen ermöglicht – ohne Geldeinlagen! Schau dir unser kurzes Video über die Schonung deiner Finanzen an und bleibe in den schwarzen Zahlen.',
   link: 'https://www.youtube.com/embed/4BRt2jU6DME',
