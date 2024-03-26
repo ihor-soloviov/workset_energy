d199e9d41ba89c52ee445400056d7aa05c0a46ed
Serhii Savchak <itsavchak@gmail.com>
2024-03-26 15:34:41 +0200

    add quest list text fix

--- src/components/home/QuestionSection/QuestionList/QuestionList.tsx
+++ src/components/home/QuestionSection/QuestionList/QuestionList.tsx
@@ -15,30 +15,30 @@ const QuestionList = () => {
     {
       title:
         'Wie viele Module sind für eine vollständige Autonomie erforderlich?',
-      text: 'Die Anzahl der Module, die für eine autonome Stromversorgung nur durch Sonnenenergie nötig sind, hängt in erster Linie von deinem persönlichen Verbrauch ab. Des Weiteren ist auch die zur Verfügung stehende Größe deines Daches ein zu berücksichtigender Faktor.Möchtest du erfahren, wie viele Module für deinen konkreten Fall nötig sind, um autonom Strom zu erzeugen? Kontaktiere uns – wir erstellen dir gern einen auf deine Bedürfnisse zugeschnittenen Plan.',
+      text: 'Die Anzahl der Module, die für eine autonome Stromversorgung nur durch Sonnenenergie nötig sind, hängt in erster Linie von deinem persönlichen Verbrauch ab. Des Weiteren ist auch die zur Verfügung stehende Größe deines Daches ein zu berücksichtigender Faktor. Möchtest du erfahren, wie viele Module für deinen konkreten Fall nötig sind, um autonom Strom zu erzeugen? Kontaktiere uns – wir erstellen dir gern einen auf deine Bedürfnisse zugeschnittenen Plan.',
     },
     {
       title: 'Wird der Einbau von PV-Anlagen staatlich gefördert?',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam nemo ut aliquam aliquid a numquam assumenda cupiditate dolorum sit hic vero dolorem, consequuntur temporibus illo enim quisquam! Repellendus est accusantium aliquam maxime culpa alias illo!',
+      text: 'Ja! Mit dem Solarpaket I will die Bundesregierung den Ausbau von Photovoltaik-/Solaranlagen weiter beschleunigen. Deshalb hat sie die jährlichen Zubauziele noch einmal angehoben: Ab 2026 soll mehr als dreimal so viel Solarenergie zugebaut werden wie bisher. Bis 2030 sollen in Deutschland Solaranlagen mit einer elektrischen Gesamtleistung von 215.000 MW auf Dächern installiert und auf Freiflächen aufgestellt sein. Wenn du mehr über die konkreten Fördermöglichkeiten deiner Region erfahren willst, kontaktiere uns!',
     },
     {
       title:
         'Gibt es Folgekosten wie etwa für Wartung und Reparatur der Anlage?',
-      text: 'Bei uns gibt es keine versteckten Kosten. Alle Kosten, die auf dich zukommen, erfährst du in unserem Kostenplan, den du unverbindlich vor der Auftragserteilung erhältst.Zudem sind unsere Anlagen so konzipiert, dass sie ohne Wartung auskommen. Einmal positioniert, produziert die Anlage ein Leben lang Strom.',
+      text: 'Bei uns gibt es keine versteckten Kosten. Alle Kosten, die auf dich zukommen, erfährst du in unserem Kostenplan, den du unverbindlich vor der Auftragserteilung erhältst. Zudem sind unsere Anlagen so konzipiert, dass sie ohne Wartung auskommen. Einmal positioniert, produziert die Anlage ein Leben lang Strom.',
     },
 
     {
       title: 'Wie lange dauert es, bis sich eine PV-Anlage amortisieren?',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt nostrum aliquam consequatur rem dolores quam ut, expedita alias mollitia dolore saepe quod facilis aspernatur nam itaque quae error cum quasi tempore, est ullam commodi molestias voluptas quaerat. Ad, aperiam saepe.',
+      text: 'Wie lange es dauert, bis sich eine PV-Anlage amortisiert, hängt von den Komponenten ab, für die du dich beim Kauf deiner PV-Anlage entschieden hast.Durchschnittlich dauert es 9-15 Jahre, bis sich deine PV-Anlage amortisiert. Mit Stromspeicher können es auch 7-13 Jahre sein – und kannst  danach noch mehr Stromkosten sparen! Kontaktiere uns, wenn du konkrete Zahlen zu deinem konkreten Fall wissen möchtest!',
     },
     {
       title: 'Worauf sollte ich beim Kauf von PV-Modulen achten?',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt sapiente, corrupti, veritatis ducimus maxime voluptate dolor tenetur nostrum earum nemo dolorem similique! Qui obcaecati nesciunt quis voluptate autem vel ea.',
+      text: 'Wichtig sind vor allem qualitativ hochwertige Produkte und ein Team aus Spezialisten, die diese Produkte sicher positionieren. Des Weiteren sind Amortisierungszeit, Anzahl der PV-Module und die Entscheidung für oder gegen einen Stromspeicher wichtig. Kontaktiere uns noch heute und unser Spezialisten-Team berät dich unverbindlich in allen Fragen, die du zu deiner künftigen PV-Anlage hast. Stelle auch gerne Vergleiche zu anderen PV-Anbietern an und entscheide dich nur für die WorkSET Energy GmbH, wenn du überzeugt davon bist, dass wir die beste Wahl für dich sind!',
     },
     {
       title:
         'Woher weiß ich, welche Art von PV-Modul für mich am besten geeignet ist?',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat reiciendis nisi animi inventore, eveniet et magnam atque sapiente dicta debitis molestiae rerum, dignissimos adipisci accusantium quas, voluptatem optio reprehenderit. Unde quisquam veritatis nulla, ex, dicta nisi illum quasi error laboriosam amet eius culpa earum ratione, vel doloremque iste obcaecati aliquam.',
+      text: 'Bei dieser Frage können dir unsere Spezialisten weiterhelfen. Kontaktiere uns für ein persönliches Beratungsgespräch und erfahre, welche Lösung für dich die beste ist!',
     },
   ];
 
