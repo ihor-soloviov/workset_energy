81ee5648dbd80a0843a7e42d44cdab511f808ab4
Ihor Soloviov <igor.musson.55@gmail.com>
2024-03-26 18:34:47 +0200

    Add new slides and sections to Wallbox page

--- src/app/wallbox/page.tsx
+++ src/app/wallbox/page.tsx
@@ -1,15 +1,31 @@
-import HeroSection from '@/components/common/HeroSection/HeroSection';
 import React from 'react';
-import { wallboxHero } from '@/utils/hero';
+import HeroSection from '@/components/common/HeroSection/HeroSection';
 import VideoSection from '@/components/common/VideoSection/VideoSection';
-import { wallboxVideo } from '@/utils/video';
 import VorteileSection from '@/components/common/ModulesVorteileSection/VorteileSection';
+import PricingPlansSection from '@/components/common/PricingPlans/PricingPlansSection';
+import EmployeeSection from '@/components/common/EmployeeSection/EmployeeSection';
+import GoogleComSection from '@/components/common/GoogleComSection/GoogleComSection';
+import TeamSection from '@/components/common/TeamSection/TeamSection';
+
+import { wallboxHero } from '@/utils/hero';
+import { wallboxVideo } from '@/utils/video';
+
+import { sliderImages, vorteileItems } from './slides';
+import ContactUsSection from '@/components/common/ContactUsSection/ContactUsSection';
+import RedHelpBlock from '@/components/common/RedHelpSection/RedHelpSection';
+
 const Wallbox = () => {
   return (
     <>
       <HeroSection {...wallboxHero} />
       <VideoSection {...wallboxVideo} />
-      {/* <VorteileSection sliderText={vorteileItems} sliderImages={sliderImages} /> */}
+      <VorteileSection sliderText={vorteileItems} sliderImages={sliderImages} />
+      <PricingPlansSection />
+      <EmployeeSection />
+      <GoogleComSection />
+      <TeamSection />
+      <ContactUsSection />
+      <RedHelpBlock />
     </>
   );
 };
---
+++ src/app/wallbox/slides.ts
@@ -0,0 +1,66 @@
+import { ImportedImages, VorteileSliderItems } from '@/types/infoTypes';
+import wbSlide1Mob from '/public/images/wallbox-page/WSlide1Mob.jpg';
+import wbSlide1Desk from '/public/images/wallbox-page/WSlide1Desk.jpg';
+import wbSlide2Mob from '/public/images/wallbox-page/WSlide2Mob.jpg';
+import wbSlide2Desk from '/public/images/wallbox-page/WSlide2Desk.jpg';
+import wbSlide3Mob from '/public/images/wallbox-page/WSlide3Mob.jpg';
+import wbSlide3Desk from '/public/images/wallbox-page/WSlide3Desk.jpg';
+import wbSlide4Mob from '/public/images/wallbox-page/WSlide4Mob.jpg';
+import wbSlide4Desk from '/public/images/wallbox-page/WSlide4Desk.jpg';
+import wbSlide5Mob from '/public/images/wallbox-page/WSlide5Mob.jpg';
+import wbSlide5Desk from '/public/images/wallbox-page/WSlide5Desk.jpg';
+import wbSlide6Mob from '/public/images/wallbox-page/WSlide6Mob.jpg';
+import wbSlide6Desk from '/public/images/wallbox-page/WSlide6Desk.jpg';
+export const vorteileItems: VorteileSliderItems[] = [
+  {
+    title: 'Wallbox-Vorteile',
+    text: 'Die Wallbox bietet eine bequeme und einfache Lösung zum Aufladen von Elektrofahrzeugen direkt in deinem Haus oder Büro. Mit modernster Technologie ermöglicht sie eine flexible und effiziente Ladung, die den Bedürfnissen der Nutzer gerecht wird. Das intelligente Design und die Benutzerfreundlichkeit der Wallbox ermöglichen eine reibungslose Integration in deine täglichen Abläufe.',
+  },
+  {
+    title: 'Intelligenz',
+    text: 'Unser Wallbox-System verfügt über intelligente Funktionen wie die Ladesteuerung über eine mobile App, Ladepläne und Energieüberwachung.',
+  },
+  {
+    title: 'Effizienz',
+    text: 'Wallbox-Ladegeräte ermöglichen ein effizientes, zeit- und energiesparendes Aufladen. Ihre innovative Technologie und hohe Leistung garantieren ein schnelles und effizientes Aufladen des Elektrofahrzeugs und sorgen für Komfort und Optimierung des Energieverbrauchs.',
+  },
+  {
+    title: 'Autonomie',
+    text: 'Zusätzlich zu den finanziellen Einsparungen profitieren Sie auch von der Einspeisevergütung.Das Wallbox-System in Kombination mit PV-Modulen bietet zusätzliche Unabhängigkeit vom systemweiten Stromnetz, insbesondere bei Stromausfällen oder in Notfällen.',
+  },
+  {
+    title: 'Ersparnisse',
+    text: 'Mit PV-Modulen kannst du deinen Strom kostenlos beziehen oder die Kosten für das Aufladen deines Elektrofahrzeugs senken, womit du langfristig viel Geld sparen kannst.',
+  },
+  {
+    title: 'Umweltfreundlichkeit',
+    text: 'Der Einsatz der Wallbox zum Aufladen eines Elektroautos außerhalb eines Hauses, das mit einer PV-Anlage betrieben wird, reduziert die Umweltbelastung erheblich, da die Energie aus einer erneuerbaren Quelle stammt.',
+  },
+];
+
+export const sliderImages: ImportedImages[] = [
+  {
+    mobile: wbSlide1Mob,
+    desktop: wbSlide1Desk,
+  },
+  {
+    mobile: wbSlide2Mob,
+    desktop: wbSlide2Desk,
+  },
+  {
+    mobile: wbSlide3Mob,
+    desktop: wbSlide3Desk,
+  },
+  {
+    mobile: wbSlide4Mob,
+    desktop: wbSlide4Desk,
+  },
+  {
+    mobile: wbSlide5Mob,
+    desktop: wbSlide5Desk,
+  },
+  {
+    mobile: wbSlide6Mob,
+    desktop: wbSlide6Desk,
+  },
+];
