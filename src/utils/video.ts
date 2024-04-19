104c85c0ec85bf7af1d7f7e512feb65543f726f3
Serhii Savchak <itsavchak@gmail.com>
2024-04-19 22:23:00 +0300

    add fix

--- src/utils/video.ts
+++ src/utils/video.ts
@@ -2,33 +2,33 @@ export const firstVideo = {
   className: 'left',
   title: 'Die Energie der Zukunft!',
   link: 'https://www.youtube.com/embed/GDfqlrJ_c8U',
-  text: 'PV-Anlagen sind der Schlüssel zum Geldsparen. In einem kurzen Video erzählt dir dein Helfer Mr. Energy genauer, wie PV-Anlagen funktionieren. Mr. Energy verrät dir, wie PV-Anlagen deinen Verbrauch senken und dir Gewinn verschaffen. Investiere eine Minute deiner Zeit und erlange den Schlüssel zu einer unabhängigen Zukunft mit WorkSET Energy.',
+  text: 'PV-Anlagen sind der Schlüssel zum Geldsparen. In einem kurzen Video erzählt dir dein Helfer Mr. Energy genauer, wie PV-Anlagen funktionieren. Mr. Energy verrät dir, wie PV-Anlagen deinen Verbrauch senken und dir Gewinn verschaffen. Investiere eine Minute deiner Zeit und erlange den Schlüssel zu einer unabhängigen Zukunft mit WorkSET Energy',
 };
 
 export const secondVideo = {
   className: 'right',
   title: 'Bleibe in den schwarzen Zahlen',
-  text: 'Erfahre, wie das Kreditsystem Einsparungen bei Photovoltaikanlagen ermöglicht – ohne Geldeinlagen! Schau dir unser kurzes Video über die Schonung deiner Finanzen an und bleibe in den schwarzen Zahlen.',
+  text: 'Erfahre, wie das Kreditsystem Einsparungen bei Photovoltaikanlagen ermöglicht – ohne Geldeinlagen! Schau dir unser kurzes Video über die Schonung deiner Finanzen an und bleibe in den schwarzen Zahlen',
   link: 'https://www.youtube.com/embed/8CAVNVcMbkE',
 };
 
 export const pvAnlagenVideo = {
   className: 'left',
   title: 'Bleibe in den schwarzen Zahlen',
-  text: 'Erfahre, wie das Kreditsystem Einsparungen mit Photovoltaik-Modulen ohne Einlagen ermöglicht! Schau dir unser kurzes Video über die Schonung deiner Finanzen an und bleibe in den schwarzen Zahlen.',
+  text: 'Erfahre, wie das Kreditsystem Einsparungen mit Photovoltaik-Modulen ohne Einlagen ermöglicht! Schau dir unser kurzes Video über die Schonung deiner Finanzen an und bleibe in den schwarzen Zahlen',
   link: 'https://www.youtube.com/embed/8CAVNVcMbkE',
 };
 
 export const stormSteicherVideo = {
   className: 'left',
   title: 'Der Stromspeicher ist die Basis für Autonomie',
-  text: 'Der Stromspeicher bietet dir eine umweltfreundliche und kostengünstige Lösung. Unsere innovativen Technologien optimieren den Einsatz von Energie, was dir erhebliche Einsparungen bei deinen Ressourcen einbringt. Von der intelligenten Steuerung des Stromverbrauchs bis hin zur effizienten Speicherung.',
+  text: 'Der Stromspeicher bietet dir eine umweltfreundliche und kostengünstige Lösung. Unsere innovativen Technologien optimieren den Einsatz von Energie, was dir erhebliche Einsparungen bei deinen Ressourcen einbringt. Von der intelligenten Steuerung des Stromverbrauchs bis hin zur effizienten Speicherung',
   link: 'https://www.youtube.com/embed/M9CIRKXwVZE',
 };
 
 export const wallboxVideo = {
   className: 'left',
   title: '0 € pro Tankvorgang!',
-  text: 'Die Wallbox ist dein Held beim Energiesparen. Hör auf, für das Betanken deiner Elektroautos zu bezahlen. Wie das funktioniert und wie du Geld sparen kannst? Mr. Energy antwortet in diesem Video ausführlich darauf. Lass dich von den Details überzeugen! Nutze diese Gelegenheit, deine Sichtweise auf das Energiesparen zu ändern.',
+  text: 'Die Wallbox ist dein Held beim Energiesparen. Hör auf, für das Betanken deiner Elektroautos zu bezahlen. Wie das funktioniert und wie du Geld sparen kannst? Mr. Energy antwortet in diesem Video ausführlich darauf. Lass dich von den Details überzeugen! Nutze diese Gelegenheit, deine Sichtweise auf das Energiesparen zu ändern',
   link: 'https://www.youtube.com/embed/V0ga0HCjic0',
 };
