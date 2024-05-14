61c5cbea058bd1fd8c198e3aa53919be63159d5b
Ihor Soloviov <igor.musson.55@gmail.com>
2024-05-14 15:17:41 +0300

    testing the path class adding

--- src/components/common/Arrow/Arrow.tsx
+++ src/components/common/Arrow/Arrow.tsx
@@ -9,7 +9,32 @@ type Props = {
 const Arrow: React.FC<Props> = ({ className }) => {
   return (
     <div className={styles.gtmArrowInner}>
-      <ArrowIcon className={`${className}`} />
+      {/* <ArrowIcon /> */}
+      <svg
+        width="22"
+        height="22"
+        viewBox="0 0 22 22"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className={`${className}`}
+      >
+        <path
+          className="gtmArrowSvgPath"
+          d="M5 11H17.8333"
+          stroke=""
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          className="gtmArrowSvgPath"
+          d="M11.4167 4L17.8334 11L11.4167 18"
+          stroke=""
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
     </div>
   );
 };
