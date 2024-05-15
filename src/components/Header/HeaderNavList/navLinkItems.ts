e75fbc1b6979fadf14cd4c9d6bf71f4b1827d524
Serhii Savchak <itsavchak@gmail.com>
2024-05-15 15:31:09 +0300

    display karrier page

--- src/components/Header/HeaderNavList/navLinkItems.ts
+++ src/components/Header/HeaderNavList/navLinkItems.ts
@@ -5,6 +5,6 @@ export const navLinkItems: NavLinkItem[] = [
   { linkTo: '/pv-anlagen', title: 'PV-Anlagen' },
   { linkTo: '/stromspeicher', title: 'Stromspeicher' },
   { linkTo: '/wallbox', title: 'Wallbox' },
-  //   { linkTo: '/karriere', title: 'Karriere' },
+  { linkTo: '/karriere', title: 'Karriere' },
   { linkTo: '/uber-uns', title: 'Über uns' },
 ];
