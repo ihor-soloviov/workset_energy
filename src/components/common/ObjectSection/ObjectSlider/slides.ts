1d80094c6a25505ca9376e733f2bcd7f5e3e8ff2
Serhii Savchak <itsavchak@gmail.com>
2024-06-07 14:22:20 +0300

    add object slides

--- src/components/common/ObjectSection/ObjectSlider/slides.ts
+++ src/components/common/ObjectSection/ObjectSlider/slides.ts
@@ -4,11 +4,27 @@ import objectSlide2Mob from '/public/images/uberuns-page/object-2-mob.jpg';
 import objectSlide3Mob from '/public/images/uberuns-page/object-3-mob.jpg';
 import objectSlide4Mob from '/public/images/uberuns-page/object-4-mob.jpg';
 import objectSlide5Mob from '/public/images/uberuns-page/object-5-mob.jpg';
+import objectSlide6Mob from '/public/images/uberuns-page/object-6-mob.jpg';
+import objectSlide7Mob from '/public/images/uberuns-page/object-7-mob.jpg';
+import objectSlide8Mob from '/public/images/uberuns-page/object-8-mob.jpg';
+import objectSlide9Mob from '/public/images/uberuns-page/object-9-mob.jpg';
+import objectSlide10Mob from '/public/images/uberuns-page/object-10-mob.jpg';
+import objectSlide11Mob from '/public/images/uberuns-page/object-11-mob.jpg';
+import objectSlide12Mob from '/public/images/uberuns-page/object-12-mob.jpg';
+import objectSlide13Mob from '/public/images/uberuns-page/object-13-mob.jpg';
 import objectSlide1Desk from '/public/images/uberuns-page/object-1-desc.jpg';
 import objectSlide2Desk from '/public/images/uberuns-page/object-2-desc.jpg';
 import objectSlide3Desk from '/public/images/uberuns-page/object-3-desc.jpg';
 import objectSlide4Desk from '/public/images/uberuns-page/object-4-desc.jpg';
 import objectSlide5Desk from '/public/images/uberuns-page/object-5-desc.jpg';
+import objectSlide6Desk from '/public/images/uberuns-page/object-6-desc.jpg';
+import objectSlide7Desk from '/public/images/uberuns-page/object-7-desc.jpg';
+import objectSlide8Desk from '/public/images/uberuns-page/object-8-desc.jpg';
+import objectSlide9Desk from '/public/images/uberuns-page/object-9-desc.jpg';
+import objectSlide10Desk from '/public/images/uberuns-page/object-10-desc.jpg';
+import objectSlide11Desk from '/public/images/uberuns-page/object-11-desc.jpg';
+import objectSlide12Desk from '/public/images/uberuns-page/object-12-desc.jpg';
+import objectSlide13Desk from '/public/images/uberuns-page/object-13-desc.jpg';
 
 export const objectItems: ObjectItem[] = [
   {
@@ -40,8 +56,76 @@ export const objectItems: ObjectItem[] = [
   },
   {
     standort: 'Rodgau',
-    textList: ['Stückzahl der Module: 25', 'Speichersystem: 10 kWh'],
+    textList: ['Speichersystem: 10 kWh', 'Stückzahl der Module: 25'],
     mobile: objectSlide5Mob,
     desktop: objectSlide5Desk,
+  },
+
+  {
+    standort: 'Wiesbaden',
+    textList: [
+      'Stückzahl der Module: 27',
+      'PV-Anlage Gesamtleistung: 12,15 kWh',
+    ],
+    mobile: objectSlide6Mob,
+    desktop: objectSlide6Desk,
+  },
+  {
+    standort: 'Waldaschaff',
+    textList: ['PV-Anlage Gesamtleistung: 8 kWh', 'Stückzahl der Module: 19'],
+    mobile: objectSlide7Mob,
+    desktop: objectSlide7Desk,
+  },
+  {
+    standort: 'Hofheim am Taunus',
+    textList: [
+      'PV-Anlage Gesamtleistung: 8,55 kWh',
+      'Stückzahl der Module: 19',
+    ],
+    mobile: objectSlide8Mob,
+    desktop: objectSlide8Desk,
+  },
+  {
+    standort: 'Raunheim',
+    textList: [
+      'PV-Anlage Gesamtleistung: 7,65 kWh',
+
+      'Stückzahl der Module: 17',
+    ],
+    mobile: objectSlide9Mob,
+    desktop: objectSlide9Desk,
+  },
+  {
+    standort: 'Darmstadt',
+    textList: [
+      'PV-Anlage Gesamtleistung: 14,4 kWh',
+      'Stückzahl der Module: 32',
+    ],
+    mobile: objectSlide10Mob,
+    desktop: objectSlide10Desk,
+  },
+  {
+    standort: 'Bickenbach',
+    textList: [
+      'PV-Anlage Gesamtleistung: 8,55 kWh',
+      'Stückzahl der Module: 19',
+    ],
+    mobile: objectSlide11Mob,
+    desktop: objectSlide11Desk,
+  },
+  {
+    standort: 'Erlensee',
+    textList: [
+      'PV-Anlage Gesamtleistung: 10,8 kWh',
+      'Stückzahl der Module: 24',
+    ],
+    mobile: objectSlide12Mob,
+    desktop: objectSlide12Desk,
+  },
+  {
+    standort: 'Friedrichsdorf',
+    textList: ['PV-Anlage Gesamtleistung: 9,9kWh', 'Stückzahl der Module: 22'],
+    mobile: objectSlide13Mob,
+    desktop: objectSlide13Desk,
   },
 ];
