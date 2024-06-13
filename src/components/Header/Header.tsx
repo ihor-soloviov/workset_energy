8dd8bad15591b4c9cdcc538a19f708c47837e2d1
Serhii Savchak <itsavchak@gmail.com>
2024-06-13 15:59:21 +0300

    remove pixel from dev

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -69,19 +69,6 @@ const Header = () => {
     setIsBlack(blackList.includes(pathname));
   }, [pathname]);
 
-  useEffect(() => {
-    import('react-facebook-pixel')
-      .then(x => x.default)
-      .then(ReactPixel => {
-        ReactPixel.init(pixelId);
-        ReactPixel.pageView();
-
-        Router.events.on('routeChangeComplete', () => {
-          ReactPixel.pageView();
-        });
-      });
-  });
-
   return (
     <>
       {(pathnames.includes(pathname) || jobPathRegex.test(pathname)) && (
